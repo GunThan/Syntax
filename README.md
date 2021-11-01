1aa14aba0dee884d6e1df1dc44da3c0e45ffce7e
GunThan <Minhdentranbinh@gamil.com>
2021-11-01 14:20:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Syntax
+# Syntax
+# Syntax
+# Syntax
---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,12 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main (String[] args) {
+		System.out.println ("HelloWorld");
+		int number;
+		String nameSchool;
+		}
+	public void show() {
+		
+	}
+}
